b045a7bcf0c67639b818b8026ae70276f1c3f181
akshayjha96 <44994161+akshayjha96@users.noreply.github.com>
2023-03-29 08:24:32 +0530

    Operators
    Task
    Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. Round the result to the nearest integer.

---
+++ operators.py
@@ -0,0 +1,33 @@
+#Operators
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'solve' function below.
+#
+# The function accepts following parameters:
+#  1. DOUBLE meal_cost
+#  2. INTEGER tip_percent
+#  3. INTEGER tax_percent
+#
+
+def solve(meal_cost, tip_percent, tax_percent):
+    # Write your code here
+    tip_amount = meal_cost * (tip_percent/100)
+    tax_amount = meal_cost * (tax_percent/100)
+    print (str(round(meal_cost+tip_amount+tax_amount)))
+
+if __name__ == '__main__':
+    meal_cost = float(input().strip())
+
+    tip_percent = int(input().strip())
+
+    tax_percent = int(input().strip())
+
+    solve(meal_cost, tip_percent, tax_percent)
+
