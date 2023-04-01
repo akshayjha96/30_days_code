b40a11f7fea59571af6b06f549b8b3280ded0a35
akshayjha96 <44994161+akshayjha96@users.noreply.github.com>
2023-04-01 08:02:26 +0530

    Classes and Inheritance with conditionals.
    You are given two classes, Person and Student, where Person is the base class and Student is the derived class. Completed code for Person and a declaration for Student are provided for you in the editor. Observe that Student inherits all the properties of Person.
    
    Complete the Student class by writing the following:

---
+++ Inheritance.py
@@ -0,0 +1,57 @@
+#You are given two classes, Person and Student, where Person is the base class and Student is the derived class. Completed code for Person and a declaration for Student are provided for you in the editor. Observe that Student inherits all the properties of Person.
+
+Complete the Student class by writing the following:
+
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
+
+class Student(Person):
+    #   Class Constructor
+    #   
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    # Write your constructor here
+    def __init__(self,firstName, lastName, idNumber,scores):
+        super().__init__(firstName, lastName, idNumber)
+        self.scores = scores
+    
+
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    #
+    # Write your function here
+    def calculate(self):
+        average = sum(self.scores)/len(self.scores)
+        if (average) in range(90,101):
+            return 'O'
+        elif int(average) in range(80,90):
+            return 'E'
+        elif int(average) in range(70,80):
+            return 'A'
+        elif int(average) in range(55,70):
+            return 'P'
+        elif int(average) in range(40,55):
+            return 'D'
+        elif int(average)<40:
+            return 'T'
+        #print(average)
+
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
