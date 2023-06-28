beecf1a2cd9667583c235eb1c7ba893eada310c7
akshayjha96 <44994161+akshayjha96@users.noreply.github.com>
2023-06-28 09:08:53 +0530

    Create Exceptions_string_to_integer_d16.py

---
+++ Exceptions_string_to_integer_d16.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+
+S = input()
+try:
+    result = int(S)
+except ValueError:
+    result = 'Bad String'
+finally:
+        
+    print(result)
